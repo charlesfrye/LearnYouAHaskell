c7bff86c1bbe058e115e459886245be638a19de9
Charles Frye <charlesfrye@berkeley.edu>
2015-10-07 10:56:56 -0700

    Middle third of pattern syntax chapter

--- syntax.hs
+++ syntax.hs
@@ -21,3 +21,36 @@ add2DVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 add2DVectors (x1,y1) (x2,y2) = (x1 + x2, y1+y2)
 
 --let xs = [(1,3), (4,3), (2,4), (5,3), (5,6), (3,1)]
+
+head' :: [a] -> a
+head' [] = error "empty list"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "The list is long. Its first two elements are: " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:tail) = 1 + length' tail
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (head:tail) = head + sum' tail
+
+capital :: String -> String
+capital "" = "Empty string, whoops!"
+capital all@(first:_) = "The first letter of " ++ all ++ " is " ++ [first] 
+
+guardTell :: (RealFloat a) => a -> String
+guardTell val
+	| val < 0  = "Number is negative."
+	| val > 0  = "Number is positive."
+	| otherwise = "Number is 0."
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+	| a > b = a
+	| True  = b
