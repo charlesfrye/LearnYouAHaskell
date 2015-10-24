34545796cb1f4388a471f3acf7fd6f8aa71b088e
Charles Frye <charlesfrye@berkeley.edu>
2015-10-24 14:40:58 -0700

    recursion

---
+++ recursion.hs
@@ -0,0 +1,11 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum' xs)
