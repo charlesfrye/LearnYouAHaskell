d754064c0db1e6a52af182c4fbcda629e9ed0055
Charles Frye <charlesfrye@berkeley.edu>
2017-05-26 14:51:19 -0500

    fun with functors

--- 11_functors/functors.hs
+++ 11_functors/functors.hs
@@ -1,20 +1,29 @@
 import Data.Char
 import Data.List
 
+--instance Functor IO' where
+--        fmap f action = do
+--            result <- action
+--           return (f result)
+
 main = do line <- fmap (intersperse '-' . reverse . map toUpper) getLine
-	  putStrLn line
+          putStrLn line
+
+main' = do line <- getLine
+           let line' = reverse line
+           putStrLn $ "You said " ++ line' ++ " backwards!"
 
 --main' = do line  <- (\xs -> intersperse '-' (reverse (map toUpper xs))) getLine
---	   putStrLn line
+--     putStrLn line
 
-instance Functor ((->) r) where
-	fmap f g = (\x -> (f (g x)))
+--instance Functor ((->) r) where
+--  fmap f g = (\x -> (f (g x)))
 
-class (Functor f) => Applicative f where
-	pure :: a -> f a
-	(<*>) :: f (a -> b) -> f a -> f b
+class (Functor f) => Applicative' f where
+    pure :: a -> f a
+    (<*>) :: f (a -> b) -> f a -> f b
 
-instance Applicative Maybe where
-	pure = Just
-	Nothing <*> _ = Nothing
-	(Just f) <*> something = fmap f something
+instance Applicative' Maybe where
+    pure = Just
+    Nothing <*> _ = Nothing
+    (Just f) <*> something = fmap f something
