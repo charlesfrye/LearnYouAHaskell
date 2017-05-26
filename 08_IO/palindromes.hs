2eb58cbaf8498796c9c546c035b717591401b6fc
Charles Frye <charlesfrye@berkeley.edu>
2017-05-26 12:02:52 -0500

    remove extra copies of IO

--- 08_IO/palindromes.hs
+++
@@ -1,5 +0,0 @@
-main = interact respondPalindromes
-
-respondPalindromes contents = unlines (map (\xs ->
-                    if isPalindrome xs then "palindrome!" else "not palindrome!") (lines contents))
-                    where isPalindrome xs = xs == reverse xs
